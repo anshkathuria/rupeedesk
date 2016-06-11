903e84ffaecf90f39191eee3163be6f5bcdcb5ce
gandharv <gandharv.garg@getpromus.com>
2016-06-11 20:07:45 +0530

    Initial Commit

---
+++ RupeeDesk/RupeeDesk/Constants/AppConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RupeeDesk.Constants
+{
+    public class AppConstants
+    {
+        public static string FIREBASE_AUTH = "3aUHYw73tkGbVSbE6DxeWXXNIJ8VHEI5cwRy0L7c";
+        public static string FIREBASE_URL = "https://fybrfb.firebaseio.com/";
+    }
+}
---
+++ RupeeDesk/RupeeDesk/Interfaces/IBaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RupeeDesk.Interfaces
+{
+    public interface IBaseModel
+    {
+        Guid Id { get; set; }
+        void SetProperty(string key, string value);
+    }
+}
